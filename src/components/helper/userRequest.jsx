96b371d18c0951bef6266dca060ecb710a18fd9b
joelbobai <90658305+joelbobai@users.noreply.github.com>
2023-08-10 03:21:24 +0100

    Update userRequest.jsx

--- src/components/helper/userRequest.jsx
+++ src/components/helper/userRequest.jsx
@@ -1,6 +1,20 @@
+// import React, { useState } from "react";
+import axios from "axios";
 import { toast } from "react-toastify";
-
+axios.defaults.withCredentials = true;
+// const isLoading = useAuthStore((state) => state.isLoading);
+// const setIsLoading = useAuthStore((state) => state.setIsLoading);
+// setIsLoading(true);
+// console.log(isLoading);
+// if (isLoading) {
+//   toast.loading("Please wait...");
+// }
+// let State = true;
+export function url() {
+  return "https://e-commerce-backend-46rw.onrender.com";
+}
 export async function reqister(inputs) {
+  // toast.loading("Please wait...");
   toast.info("Please wait...", {
     position: "top-left",
     autoClose: false,
@@ -11,23 +25,10 @@ export async function reqister(inputs) {
     progress: undefined,
     theme: "light",
   });
-
-  try {
-    const response = await fetch("https://backend-e-commerce-558w5gv0q-joelbobai.vercel.app/api/v1/user/signup", {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      credentials: 'include', // Equivalent to axios withCredentials: true
-      body: JSON.stringify(inputs),
-    });
-
-    if (response.ok) {
-      const data = await response.json();
-      return data;
-    } else {
-      const errorData = await response.json();
-      toast.error(errorData, {
+  const res = await axios
+    .post(`${url()}/api/v1/user/signup`, inputs)
+    .catch((err) => {
+      toast.error(err.response.data, {
         position: "top-center",
         autoClose: 5000,
         hideProgressBar: false,
@@ -37,53 +38,18 @@ export async function reqister(inputs) {
         progress: undefined,
         theme: "light",
       });
-      console.log(errorData);
-    }
-  } catch (err) {
-    toast.error(err.message, {
-      position: "top-center",
-      autoClose: 5000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-      theme: "light",
+      console.log(err.response.data);
     });
-    console.log(err.message);
+
+  if (res) {
+    const data = await res;
+    return data;
   }
 }
-
 export async function login(inputs) {
-  try {
-    const response = await fetch("https://backend-e-commerce-558w5gv0q-joelbobai.vercel.app/api/v1/user", {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      credentials: 'include', // Equivalent to axios withCredentials: true
-      body: JSON.stringify(inputs),
-    });
-
-    if (response.ok) {
-      const data = await response.json();
-      return data;
-    } else {
-      const errorData = await response.json();
-      toast.error(errorData, {
-        position: "top-center",
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "light",
-      });
-      console.log(errorData);
-    }
-  } catch (err) {
-    toast.error(err.message, {
+ 
+  const res = await axios.post(`${url()}/api/v1/user`, inputs).catch((err) => {
+    toast.error(err.response.data, {
       position: "top-center",
       autoClose: 5000,
       hideProgressBar: false,
@@ -93,6 +59,11 @@ export async function login(inputs) {
       progress: undefined,
       theme: "light",
     });
-    console.log(err.message);
+    console.log(err.response.data);
+  });
+
+  if (res) {
+    const data = await res;
+    return data;
   }
 }
